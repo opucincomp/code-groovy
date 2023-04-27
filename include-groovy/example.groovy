b29906a70f76570d51cf80f4b21c378a48a93459
opucincomp <samadislam187@gmail.com>
2023-04-27 20:36:03 +0530

    changes

--- include-groovy/example.groovy
+++ include-groovy/example.groovy
@@ -1,9 +1,16 @@
 def example1() {
   println 'Hello from example1'
+  job("test"){
+    steps{
+      shell('ls -lrt')
+    }
+  }
 }
 
 def example2() {
   println 'Hello from example2'
 }
+
+
 
 return this
