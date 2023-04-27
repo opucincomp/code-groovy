b810d11924928edad9ecf77f8814c1054f038349
opucincomp <samadislam187@gmail.com>
2023-04-27 20:55:44 +0530

    changes

--- include-groovy/example.groovy
+++ include-groovy/example.groovy
@@ -1,12 +1,12 @@
 def example1() {
   println 'Hello from example1'
-def sout = new StringBuffer(), serr = new StringBuffer()
+  def sout = new StringBuffer(), serr = new StringBuffer()
 // 1) 
-def proc ='ls -lrt'.execute()
+  def proc ='ls -lrt'.execute()
 
-proc.consumeProcessOutput(sout, serr)
-proc.waitForOrKill(1000)
-return sout.tokenize()
+  proc.consumeProcessOutput(sout, serr)
+  proc.waitForOrKill(1000)
+  return sout.tokenize()
 }
 
 def example2() {
